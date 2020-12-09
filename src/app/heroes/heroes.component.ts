2b72bdf3afb7e236fa3c0de870354680b252f449
Romain <rhaudry@gmail.com>
2020-12-09 15:39:30 +0100

    TP4 Final Commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,12 +11,17 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
 
   constructor() { }
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   ngOnInit(): void {
+
   }
 }
 
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Superman' },
+  { id: 2, name: 'Petru' },
+  { id: 3, name: 'Maxence' },
+  { id: 4, name: 'Nicolas Bem' },
+  { id: 5, name: 'Stickos' },
+  { id: 6, name: 'Nobuki' },
+  { id: 7, name: 'Mectose' },
+  { id: 8, name: 'E36' },
+  { id: 9, name: 'Sapin de noel' },
+  { id: 0, name: 'l argent'}
+];
