001cdab7232cc36e1bf0f2c605e02b04ea393375
Romain <rhaudry@gmail.com>
2020-12-09 16:33:11 +0100

    TP6 Avant component message

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,18 +11,19 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
-  heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  constructor(private heroService: HeroService) {}
+  heroes: Hero[];
+
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  ngOnInit(): void {
-
+  ngOnInit() {
+    this.getHeroes();
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
 
